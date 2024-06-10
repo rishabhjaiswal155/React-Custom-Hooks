413dad80485f854d5096db23882472e3241e224c
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-06-10 20:50:49 +0530

    Custom hook(to avoid boilerplate code,create custom hook and provide shaed logic there) useDocTitle custom  hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import DocTitleOne from './components/DocTitleOne';
+import DocTitleTwo from './components/DocTitleTwo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DocTitleOne/><br/>
+      <DocTitleTwo/>
     </div>
   );
 }
